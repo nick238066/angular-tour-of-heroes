32686dc3d3cc6d0a74ec1f4518a27bbab135274b
nick238066 <38974877+nick238066@users.noreply.github.com>
2018-07-22 23:00:58 +0800

    2018/07/22

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-// import { HEROES } from './mock-heroes';
+import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -18,19 +19,38 @@ export class HeroService {
   ) { }
 
   getHeroes(): Observable<Hero[]>{
-  	// return of(HEROES);
-  	// this.messageService.add('HeroService: fetched heros');
-  	// return of(HEROES);
-  	return this.http.get<Hero[]>(this.heroesUrl);
+  	return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(heroes=>this.log(`fetched heroes`)),
+        catchError(this.handleError('getHeros',[]))
+      );
   }
 
   getHero(id: number): Observable<Hero> {
-  	this.messageService.add(`HeroService: fetched hero id=${id}`);
-  	return of(HEROES.find(hero=>hero.id===id));
+    const url = `${this.heroesUrl}/${id}`;
+  	//this.messageService.add(`HeroService: fetched hero id=${id}`);
+  	return this.http.get<Hero>(url).pipe(
+      tap(_=>this.log(`fetched hero id=${id}`)),
+      catchError(this.headleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   private log(message: string){
   	this.messageService.add('HeroService: ' + message);
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+   
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+   
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+   
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
 }
